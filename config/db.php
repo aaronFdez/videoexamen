87251974d61d0d4e1b0dd542d1efc5d53bd2d3b8
Aarón Fernández <aaronfdezgarcia@gmail.com>
2016-12-14 20:58:46 +0100

    carga inicial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=videoExamen',
+    'username' => 'videoExamen',
+    'password' => 'videoExamen',
+    'charset' => 'utf8',
+];
